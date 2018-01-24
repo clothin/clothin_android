629bd50272bce020f5c05d043fe7c024288c3635
anze <nzkozar@gmail.com>
2018-01-24 02:11:47 +0100

    Implemented Product object creation from blockchain data

--- app/src/main/java/si/cit/clothingorigin/Objects/Producer.java
+++ app/src/main/java/si/cit/clothingorigin/Objects/Producer.java
@@ -8,19 +8,23 @@ package si.cit.clothingorigin.Objects;
 public class Producer {
 
     public long id;
-    public String name, country, service;
+    public String name, country, service, maps_url;
+
+    public Producer(){}
+
+    public Producer(String name,String country,String service,String maps_url){
+        this.name = name;
+        this.country = country;
+        this.service = service;
+        this.maps_url = maps_url;
+    }
 
     public static Producer fakeProducer(int index){
         String[] fakeCountries = {"China","Iran","USA","Philipines","Slovenia"};
         String[] fakeServices = {"Sewing","Cotton manufacturing","Coloring & Printing","Packaging","Retail"};
-
         if(index<0 || index>fakeCountries.length-1)index = 0;
-        Producer producer = new Producer();
-        producer.name = "Producer "+index;
-        producer.country = fakeCountries[index];
-        producer.service = fakeServices[index];
 
-        return producer;
+        return new Producer("Producer "+index,fakeCountries[index],fakeServices[index],"");
     }
 
 }
--- app/src/main/java/si/cit/clothingorigin/Objects/Product.java
+++ app/src/main/java/si/cit/clothingorigin/Objects/Product.java
@@ -1,7 +1,16 @@
 package si.cit.clothingorigin.Objects;
 
+import com.google.gson.Gson;
+
+import org.web3j.tuples.generated.Tuple2;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import si.cit.clothingorigin.CitApp;
+import si.cit.clothingorigin.Interfaces.ContractResultListener;
+import si.cit.clothingorigin.Interfaces.ObjectDataChangeListener;
+import timber.log.Timber;
 
 /**
  * Created by Anže Kožar on 22.1.2018.
@@ -10,9 +19,40 @@ import java.util.List;
 
 public class Product {
 
+    public static final int REQUEST_CODE_PRODUCTION_CHAIN_FETCH = 0;
+
     public long id;
-    public String title, size, color, price, picture_url, sold_by, materials, weight;
-    public List<Producer> production_chain;
+    public String title, size, color, price, picture_url, sold_by, materials;
+    public List<Producer> production_chain = new ArrayList<>();
+
+    public void fetchProductionChain(final ObjectDataChangeListener listener){
+        CitApp.getInstance().getBlockchainConnector().getProductProductionChainInfo(this.id, new ContractResultListener() {
+            @Override
+            public void onContractResult(Object resultObject, boolean success) {
+                if(success) {
+                    Tuple2 tuple2 = (Tuple2) resultObject;
+                    String trailString = (String)tuple2.getValue1();
+                    String[] trailNodeStrings = trailString.split(";");
+                    for (String s:trailNodeStrings) {
+                        String[] producerValues = s.split(":");
+                        Producer producer = new Producer(
+                                producerValues[1],
+                                producerValues[0],
+                                producerValues[2],
+                                producerValues[4]+":"+producerValues[5]);
+                        production_chain.add(producer);
+                        Gson gson = new Gson();
+                        Timber.i("Product trail node: " + gson.toJson(producer));
+                    }
+                    //Gson gson = new Gson();
+                    //Timber.i("Product trail result: " + gson.toJson(tuple2));
+                    listener.onDataChange(this);
+                }else{
+                    Timber.i("Product trail result failed!");
+                }
+            }
+        });
+    }
 
     public static Product fakeProduct0(){
         Product product = new Product();
@@ -24,12 +64,11 @@ public class Product {
         product.materials = "Cotton (98%), Polyester (2%)";
         product.picture_url = "http://lp.hm.com/hmprod?set=source%5B%2Fenvironment%2F2017%2F10J_0636_003R.jpg%5D%2Cmedia_type%5BFASHION_FRONT%5D%2Ctshirt_size%5BL%5D%2Cquality%5BH%5D%2Csr_x%5B0%5D%2Csr_y%5B-1%5D%2Csr_height%5B4114%5D%2Csr_width%5B3519%5D%2Chmver%5B0%5D&call=url%5Bfile%3A%2Flegacy%2Fv1%2Fproduct.chain%5D";
 
-        List<Producer> producerChain = new ArrayList<>();
-        producerChain.add(Producer.fakeProducer(0));
-        producerChain.add(Producer.fakeProducer(1));
-        producerChain.add(Producer.fakeProducer(2));
-        producerChain.add(Producer.fakeProducer(3));
-        producerChain.add(Producer.fakeProducer(4));
+        product.production_chain.add(Producer.fakeProducer(0));
+        product.production_chain.add(Producer.fakeProducer(1));
+        product.production_chain.add(Producer.fakeProducer(2));
+        product.production_chain.add(Producer.fakeProducer(3));
+        product.production_chain.add(Producer.fakeProducer(4));
 
         return product;
     }
